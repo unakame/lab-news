9718db4ac5ab1a4bb49461b390aae2261ed54f28
Dana Franco <turquoise.ilg@hotmail.com>
2017-07-07 21:32:06 -0500

    hidden

--- public/assets/js/components/footer.js
+++ public/assets/js/components/footer.js
@@ -2,7 +2,7 @@
 
 const Footer = () => {
 
-  const footer = $('<footer><div class="row container-img-tec"><div class="col-md-4">© Editado por Laboratoria Av. José Pardo #601 Lima 1 PerúCopyright © Laboratoria.la Todos los derechos reservados.</div><div class="col-md-4">DIRECTOR GENERAL:Francisco Miró Quesada Cantuarias DIRECTOR PERIODÍSTICO:Fernando Berckemeyer Olaechea SUSCRIPCIONES:suscriptores@comercio.com.pe PUBLICIDAD ONLINE:fonoavisos@comercio.com.pe CLUB EL COMERCIO:clubelcomercio@comercio.com.peCompromiso de Autorregulación Comercial</div><div class="col-md-4">TÉRMINOS Y CONDICIONES DE USOTRABAJA CON NOSOTROS LIBRO DE RECLAMACIONES SEDES PRINCIPALESPOLÍTICAS DE PRIVACIDAD</div></footer>');
+  const footer = $('<footer><div class="row container-img-tec"><div class="col-md-4">© Editado por Laboratoria Av. José Pardo #601 Lima 1 PerúCopyright © Laboratoria.la Todos los derechos reservados.</div><div class="col-md-4 hidden-xs">DIRECTOR GENERAL:Francisco Miró Quesada Cantuarias DIRECTOR PERIODÍSTICO:Fernando Berckemeyer Olaechea SUSCRIPCIONES:suscriptores@comercio.com.pe PUBLICIDAD ONLINE:fonoavisos@comercio.com.pe CLUB EL COMERCIO:clubelcomercio@comercio.com.peCompromiso de Autorregulación Comercial</div><div class="col-md-4 hidden-xs">TÉRMINOS Y CONDICIONES DE USOTRABAJA CON NOSOTROS LIBRO DE RECLAMACIONES SEDES PRINCIPALESPOLÍTICAS DE PRIVACIDAD</div></footer>');
 
 return footer;
 
