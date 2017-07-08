01b530004d5c9c8fcd35b666b1f12165d21dff37
Dana Franco <turquoise.ilg@hotmail.com>
2017-07-07 20:44:07 -0500

    opinion

--- public/assets/js/components/opinion.js
+++ public/assets/js/components/opinion.js
@@ -5,9 +5,12 @@ const Opinion = () => {
   const sectionOpi   = $('<div class="tec"></div>');
   const educ         = $('<p class="tecno"><strong>OPINION</strong></p>');
   const hrM          = $('<hr class="hrm">');
+  const divImgOp     = $('<div class="row container-img-tec"><div class="col-md-3 news-24"><div class= "container-color-m-8"> Titulo noticia laboratoria </div></div><div class="col-md-3 news-25"><div class= "container-color-m-8"> Titulo noticia laboratoria </div></div><div class="col-md-3 news-26"><div class= "container-color-m-8"> Titulo noticia laboratoria </div></div><div class="col-md-3 news-27"><div class= "container-color-m-8"> Titulo noticia laboratoria </div></div></div>')
 
    sectionOpi.append(educ);
    sectionOpi.append(hrM);
+   sectionOpi.append(divImgOp);
+
 
 
   return sectionOpi;
