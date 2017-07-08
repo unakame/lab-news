2feec5fc9c59c0277e47b81948a2c593ce8b243a
Dana Franco <turquoise.ilg@hotmail.com>
2017-07-07 20:57:41 -0500

    footer letter

--- public/assets/js/components/footer.js
+++ public/assets/js/components/footer.js
@@ -2,7 +2,7 @@
 
 const Footer = () => {
 
-  const footer = $('<footer></footer>');
+  const footer = $('<footer><div class="row container-img-tec"><div class="col-md-4">Laboratoria</div><div class="col-md-4">Director General: Francisco Miró Quesada Canutuarias</div><div class="col-md-4">Términos y condiciones de uso</div></footer>');
 
 return footer;
 
