07c75eec745000d5e5e3f49d6ca3d07337402e4c
Dana Franco <turquoise.ilg@hotmail.com>
2017-07-07 21:23:06 -0500

    carouelll

--- public/assets/js/components/carousel.js
+++ public/assets/js/components/carousel.js
@@ -2,7 +2,7 @@
 
 
 const Carousel = () => {
-const carousel = $('<div class="owl-carousel owl-theme">  <div class="item"><h4>1</h4></div>  <div class="item"><h4>2</h4></div>  <div class="item"><h4>3</h4></div>  <div class="item"><h4>4</h4></div>  <div class="item"><h4>5</h4></div><div class="item"><h4>6</h4></div>  <div class="item"><h4>7</h4></div><div class="item"><h4>8</h4></div><div class="item"><h4>9</h4></div>  <div class="item"><h4>10</h4></div>  <div class="item"><h4>11</h4></div>  <div class="item"><h4>12</h4></div></div>');
+const carousel = $('<div id="myCarousel" class="carousel slide" data-ride="carousel"><ol class="carousel-indicators"><li data-target="#myCarousel" data-slide-to="0" class="active"></li><li data-target="#myCarousel" data-slide-to="1"></li><li data-target="#myCarousel" data-slide-to="2"></li>  </ol>  <!-- Wrapper for slides --><div class="carousel-inner">  <div class="item active">  <img src="assets/img/news/news-0.png"></div><div class="item">  <img src="assets/img/news/news-0.png">  </div><div class="item"><img src="ny.jpg">  </div></div><a class="left carousel-control" href="#myCarousel" data-slide="prev">  <span class="glyphicon glyphicon-chevron-left"></span>  <span class="sr-only">Previous</span></a><a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span>  <span class="sr-only">Next</span>  </a></div>');
 
 return carousel;
 }
